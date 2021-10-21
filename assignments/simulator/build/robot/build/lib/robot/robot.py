c465c3433f53fabfd9c56ecd7756d3b123a3a958
Martin Jenigár <martin.jenigar@student.tuke.sk>
2021-10-21 07:03:42 +0200

    ros2 package

---
+++ assignments/simulator/build/robot/build/lib/robot/robot.py
@@ -0,0 +1,92 @@
+import sys
+import rclpy
+
+import numpy as np
+
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class Robot(Node):
+    def __init__(self, pos, name="Isaac"):
+        super().__init__(name)
+        self.name = name
+        self.robot = None
+        self.world = None
+        self.pos = np.array(pos)
+        self.velocity = np.array([0.01, 0])
+        
+        self.size = 12
+        
+        ### Flags
+        self.connected = False
+        
+        ### Listeners
+        self.world_conn_listener = self.create_subscription(String, "world_connection", self.Connect2World, 10)
+        self.forward_listener = self.create_subscription(String, "{}_forward".format(self.name), self.MoveForward, 10)
+        self.backward_listener = self.create_subscription(String, "{}_backward".format(self.name), self.MoveBackward, 10)
+
+        ### Timers
+        self.timer = self.create_timer(1, self.Search4World)
+
+    def PublishStrMsg(self, topic, _msg):
+        pub = self.create_publisher(String, topic, 10)
+        msg = String()
+        msg.data = _msg
+        pub.publish(msg)
+        self.get_logger().info("sent [{} Bytes] --> {}".format(sys.getsizeof(msg.data), msg.data))
+        self.destroy_publisher(pub)
+    
+    
+    def Search4World(self):
+        self.conn_req = self.create_publisher(String, "world_conn_req", 10)
+        msg = String()
+        msg.data = "{}_{}-{}".format(self.name, self.pos[0], self.pos[1])
+        self.conn_req.publish(msg)
+        self.get_logger().info("sent [{} Bytes] --> {}".format(sys.getsizeof(msg.data), msg.data))
+        # self.destroy_publisher(self.conn_req)
+    
+    def Connect2World(self, msg):
+        self.connected = True
+        self.timer.destroy()
+        
+        print("Connected...")
+
+    def Spawn(self, canvas, pos):
+        size = self.Coord2Size(pos)
+        self.robot = canvas.create_rectangle(size[0], size[1], size[2], size[3], fill="red")
+
+    def Update(self):
+        self.robot.delete("all")
+        self.robot = self.world.create_rectangle(event.x + 5,event.y + 5, position)
+    
+    def Coord2Size(self, pos):
+        x_ = pos[0] + self.size
+        y_ = pos[1] + self.size
+
+        return np.array([pos[0], pos[1], x_, y_])
+    
+    def MoveForward(self, msg):
+        self.pos = np.add(self.pos, self.velocity)
+        msg = "{}_{}-{}".format(self.name, self.pos[0], self.pos[1])
+        self.PublishStrMsg("robot_upd", msg)
+        # print(self.pos)
+        
+    def MoveBackward(self, msg):
+        self.pos = np.subtract(self.pos, self.velocity)
+        msg = "{}_{}-{}".format(self.name, self.pos[0], self.pos[1])
+        self.PublishStrMsg("robot_upd", msg)
+        # print(self.pos)
+        
+
+def main():
+    rclpy.init()
+
+    isaac = Robot([0,0])
+    rclpy.spin(isaac)
+    
+    isaac.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ assignments/simulator/build/simulator/build/lib/simulator/simulator.py
@@ -0,0 +1,265 @@
+import sys
+import json
+import math
+import threading
+import tkinter as tk
+import numpy as np
+
+import rclpy
+from rclpy.node import Node
+from rclpy.task import Future
+from std_msgs.msg import String
+
+#TODO create logs
+#TODO create edit mode
+#TODO https://stackoverflow.com/questions/17466561/best-way-to-structure-a-tkinter-application
+
+
+class Simulator(Node, tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__("Simulator")
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.robots = []
+        self.active_robot = None
+        self.map_size = {}
+        self.cell_size = None
+        self.bit_map = None
+        self.cells = {}
+        
+        ### Flags
+        self.map_cfg_flag = False
+        self.map_flag = False
+        self.robot_flag = False
+        
+        ### Listeners
+        self.listener_thread = threading.Thread(target=self.StartListening)        
+        self.map_cfg_listener = self.create_subscription(String, "map_cfg", self.HandleMapCfg, 10)
+        self.map_listener = self.create_subscription(String, "map", self.HandleMap, 10)
+        self.robot_pos_listener = self.create_subscription(String, "robots", self.HandleRobots, 10)
+        self.robot_pos_update_listener = self.create_subscription(String, "robot_pos_update", self.UpdatePosition, 10)
+        
+        ### Tasks
+        self.init = Future()   
+        
+        ### Timers
+        self.init_timer = self.create_timer(1, self.InitSimulator)
+
+        ### TMP
+        self.velocity = np.array([1, 0])
+        
+    def InitSimulator(self):
+        if self.map_cfg_flag and self.map_flag and self.robot_flag:
+            self.get_logger().info("Starting simulator ...")
+            self.DrawMap()        
+            self.init.set_result("OK")
+            self.init_timer.destroy()
+            self.listener_thread.start()
+            
+            pub = self.create_publisher(String, "sim_opened", 10)
+            msg = String()
+            msg.data = "sim_ok"
+            pub.publish(msg)
+            self.get_logger().info("sent [{} Bytes] --> {}".format(sys.getsizeof(msg.data), msg.data))
+            self.destroy_publisher(pub)
+            
+            self.active_robot = self.robots[0]
+            self.mainloop()
+        else:
+            self.get_logger().info("Waiting for data ...")
+    
+    ### LISTENER HANDLERS
+    def StartListening(self):
+        print("Listening started ... ")
+        rclpy.spin(self)
+    
+    def HandleMapCfg(self, msg):
+        s = msg.data.split("/")
+        self.cell_size = int(s[1])
+        self.map_size["w"] = int(s[0].split("x")[0])
+        self.map_size["h"] = int(s[0].split("x")[1])
+        
+        self.canvas = tk.Canvas(self, 
+                                width=self.map_size["w"],
+                                height=self.map_size["h"],
+                                borderwidth=0,
+                                highlightthickness=0)
+        self.canvas.pack(side="top", fill="both", expand="true")
+
+        ### Mouse events
+        self.canvas.bind('<Button-1>', self.LeftClick)
+        self.canvas.bind('<Button-2>', self.MiddleClick)
+        self.canvas.bind('<Button-3>', self.RightClick)
+        
+        ### Controls events
+        self.bind('<Left>', self.Left)           
+        self.bind('<Right>', self.Right)           
+        self.bind('<Up>', self.Up)           
+        self.bind('<Down>', self.Down)           
+                
+        self.n_cells = int(self.map_size["w"]/self.cell_size)
+        self.map_cfg_flag = True
+        self.get_logger().info("I got: {}".format(msg.data))
+        self.get_logger().info("Map Config - OK")
+        
+    def HandleMap(self, msg):
+        self.get_logger().info("I got: {}".format(msg.data[0:25]))
+        self.bit_map = msg.data
+        self.map_flag = True
+        self.get_logger().info("Map - OK")
+
+    def HandleRobots(self, msg):
+        avail_robots = msg.data.split(",")
+        for robot in avail_robots:
+            robot_data = robot.split("_")
+            newborn = {
+                "name": robot_data[0],
+                "size": 12,
+                "x": int(robot_data[1].split("-")[0]),
+                "y": int(robot_data[1].split("-")[1]),
+                "item" : None
+            }
+            
+            if len(self.robots) > 0:
+                for other in self.robots:
+                    if other["name"] != newborn["name"]:
+                        self.robots.append(newborn)           
+                    else:
+                        return
+            else:
+                self.robots.append(newborn)
+                
+        if len(self.robots) > 0:
+            self.robot_flag = True
+            self.get_logger().info("Robots - OK")
+
+        self.get_logger().info("I got: {}".format(msg))
+        
+        
+    ### PUBLISHER HANDLERS
+    def PublishStringMsg(self, topic, msg_):
+        msg = String()
+        pub = self.create_publisher(String, topic, 10)
+        msg.data = msg_
+        pub.publish(msg)
+        self.get_logger().info("{} [{} Bytes] --> {}".format(topic, sys.getsizeof(msg.data), msg.data[0:25] if len(msg.data) > 25 else msg.data))
+        self.destroy_publisher(pub)
+    
+    def PublishMapUpdate(self):
+        msg = String()
+        self.pub = self.create_publisher(String, "map_upd", 10)
+        msg.data = self.bit_map
+        self.pub.publish(msg)
+        self.get_logger().info("sent [{} Bytes] --> {}".format(sys.getsizeof(msg.data), msg.data[0:25] if len(msg.data) > 25 else msg.data))
+        self.destroy_publisher(self.pub)
+        
+    def GetMapMatrix(self):
+        x = 0
+        self.map_matrix = [] 
+        for x in range(self.n_cells):
+            self.map_matrix.append(self.bit_map[x:x+self.n_cells])
+
+        return self.map_matrix
+    
+    def Matrix2Bin(self):
+        self.bit_map = ""
+        for x in range(self.n_cells):
+            for y in range(self.n_cells):
+                self.bit_map += self.map_matrix[x][y]
+        
+        # self.bit_map = ''.join(self.map_matrix)
+    
+    def DrawMap(self):
+        self.GetMapMatrix()
+        
+        for x in range(len(self.map_matrix)):
+            for y in range(len(self.map_matrix[x])):
+                x1 = x * self.cell_size
+                x2 = x1 + self.cell_size
+                
+                y1 = y * self.cell_size
+                y2 = y1 + self.cell_size
+                
+                color = "black" if self.map_matrix[x][y] == "1" else "white"
+                self.cells[x, y] = self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, tags="cell")
+                self.canvas.tag_lower(self.cells[x, y])
+                for robot in self.robots:
+                    if robot["x"] == x and robot["y"] == y:
+                        start_x = robot["x"] * self.cell_size
+                        end_x = start_x + robot["size"]
+                        
+                        start_y = robot["y"] * self.cell_size
+                        end_y = start_y + robot["size"]
+                        
+                        robot["item"] = self.canvas.create_rectangle(start_x, start_y, end_x, end_y, fill='red')
+                        self.canvas.tag_raise(robot["item"])
+    
+    ### EVENT CONTROLS
+    def LeftClick(self, event):
+        x = math.floor(event.x / self.cell_size)
+        y = math.floor(event.y / self.cell_size)
+        print("clicked at x: {} y: {}".format(x, y))
+        self.canvas.itemconfig(self.cells[(x, y)], fill="black")        
+        tmp = list(self.map_matrix[x])
+        tmp[y] = '1'
+        print(tmp)
+        
+        self.map_matrix[x] = ''.join(tmp)
+        self.Matrix2Bin()
+        self.PublishMapUpdate()        
+        
+        print(self.map_matrix)
+        
+    
+    def MiddleClick(self, event):
+        #TODO bind mouse event        
+        print("middle")
+
+    def RightClick(self, event):
+        #TODO bind mouse event        
+        print("right")
+        
+    def Right(self, event):
+        # self.PublishStringMsg("move_forward", self.active_robot["name"])
+        self.canvas.move(self.robots[0]["item"], self.robots[0]["x"] +  self.velocity[0], self.robots[0]["y"] + self.velocity[1])
+        
+    def Left(self, event):
+        # self.PublishStringMsg("move_backward", self.active_robot["name"])
+        self.canvas.move(self.robots[0]["item"], self.robots[0]["x"] -  self.velocity[0], self.robots[0]["y"] - self.velocity[1])
+
+    def Up(self, event):
+        # TODO rotation up
+        new_vel = [self.velocity[0], self.velocity[1] - 0.1]
+        self.velocity = np.array(new_vel)
+        print("Rotating UP Current {}".format(self.velocity[1]))
+        
+    def Down(self, event):
+        # TODO rotation down
+        new_vel = [self.velocity[0], self.velocity[1] + 0.1]
+        self.velocity = np.array(new_vel)
+        print("Rotating DOWN Current {}".format(self.velocity[1]))
+
+    ### Move handlers
+    def UpdatePosition(self, msg):
+        self.get_logger().info("sent [{} Bytes] --> {}".format(sys.getsizeof(msg.data), msg.data[0:25] if len(msg.data) > 25 else msg.data))
+        raw = msg.data.split("_")
+        robot2upd = raw[0]
+        for robot in self.robots:
+            if robot["name"] == robot2upd:
+                robot["x"] = float(raw[1].split("-")[0])
+                robot["y"] = float(raw[1].split("-")[1])
+
+                self.canvas.move(robot["item"], robot["x"], robot["y"])
+            
+
+def main():
+    rclpy.init()
+    l = Simulator()
+    # l.listener_thread.start()
+    rclpy.spin_until_future_complete(l, l.init)
+    
+    l.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
