d13e9b083077ab409885c3666848c0adceb7dc69
Martin Jenigár <martin.jenigar@student.tuke.sk>
2021-11-11 13:30:20 +0100

    cv8

---
+++ gotogoal.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+import rclpy
+
+from rclpy.node import Node
+
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+from math import pow, atan2, sqrt
+
+class TurtleBot(Node):
+    def __init__(self):
+        super().__init__("turtlebot_controlle")
+
+        self.velocity_publisher = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.pose_subscriber = self.create_subscription(Pose, '/turtle1/pose', self.update_pose, 10)
+
+        self.pose = Pose()
+        # self.rate = rclpy.rate(10)
+        self.rate = self.create_rate(10, self.get_clock())
+
+    def update_pose(self, data):
+        """Callback function which is called when a new message of type Pose is
+        received by the subscriber."""
+        self.pose = data
+        self.pose.x = round(self.pose.x, 4)
+        self.pose.y = round(self.pose.y, 4)
+
+    def euclidean_distance(self, goal_pose):
+        """Euclidean distance between current pose and the goal."""
+        return sqrt(pow((goal_pose.x - self.pose.x), 2) +
+                    pow((goal_pose.y - self.pose.y), 2))
+
+    def manhathan_distance(self, goal_pose):
+        return (goal_pose.x - self.pose.x) + (goal_pose.y - self.pose.y)
+    
+    def linear_vel(self, goal_pose, constant=1.5):
+        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
+        return constant * self.euclidean_distance(goal_pose)
+
+    def steering_angle(self, goal_pose):
+        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
+        return atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
+
+    def angular_vel(self, goal_pose, constant=6):
+        """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
+        return constant * (self.steering_angle(goal_pose) - self.pose.theta)
+
+    def move2goal(self):
+        """Moves the turtle to the goal."""
+        goal_pose = Pose()
+
+        # Get the input from the user.
+        goal_pose.x = float(input("Set your x goal: "))
+        goal_pose.y = float(input("Set your y goal: "))
+
+        # Please, insert a number slightly greater than 0 (e.g. 0.01).
+        distance_tolerance = float(input("Set your tolerance: "))
+
+        vel_msg = Twist()
+
+        # while self.euclidean_distance(goal_pose) >= distance_tolerance:
+        while self.manhathan_distance(goal_pose) >= distance_tolerance:
+        
+            # Porportional controller.
+            # https://en.wikipedia.org/wiki/Proportional_control
+
+            # Linear velocity in the x-axis.
+            vel_msg.linear.x = self.linear_vel(goal_pose)
+            vel_msg.linear.y = 0.0
+            vel_msg.linear.z = 0.0
+
+            # Angular velocity in the z-axis.
+            vel_msg.angular.x = 0.0
+            vel_msg.angular.y = 0.0
+            vel_msg.angular.z = self.angular_vel(goal_pose)
+
+            # Publishing our vel_msg
+            self.velocity_publisher.publish(vel_msg)
+
+            # Publish at the desired rate.
+            self.rate.sleep()
+
+        # Stopping our robot after the movement is over.
+        vel_msg.linear.x = 0
+        vel_msg.angular.z = 0
+        self.velocity_publisher.publish(vel_msg)
+
+        # If we press control + C, the node will stop.
+        rospy.spin()
+
+if __name__ == '__main__':
+    rclpy.init()
+    
+    x = TurtleBot()
+    x.move2goal()
